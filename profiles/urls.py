3c0841d64c309c55f2dcc34be928264669b5af7e
arwindpianist <arwindpianist@gmail.com>
2023-08-11 01:15:26 +0800

    updated views

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     # Existing URL pattern for the profile list view
     path('api/profiles/', views.profile_list, name='profile-list'),
-    
+    # Add this to your urls.py
+    path('api/create-profile/', views.create_profile, name='create-profile'),
     # New URL pattern for the QR code generator
     path('api/generate-qr-code/', views.generate_qr_code, name='generate-qr-code'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,35 +1,90 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+# views.py
+
+import json
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views import View
 import qrcode
 import io
 from PIL import Image
 from .models import UserProfile
 
-def generate_qr_code(data):
-    # Create a QR code instance
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
+class CreateProfileView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
-    # Convert the image to a BytesIO object
-    qr_img = io.BytesIO()
-    img.save(qr_img, "PNG")
-    return qr_img.getvalue()
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+            instagram = data.get('instagram')
+            twitter = data.get('twitter')
+            email = data.get('email')
+            phone_number = data.get('phone_number')
 
-def profile_list(request):
-    profiles = get_list_or_404(UserProfile)
-    data = []
-    for profile in profiles:
-        qr_code_data = f"https://example.com/profile/{profile.id}/"  # Replace with the actual URL for the profile
-        qr_code_image = generate_qr_code(qr_code_data)
-        data.append({
-            "name": profile.name,
-            "qr_code": qr_code_image,
-        })
-    return JsonResponse(data, safe=False)
+            # Create the user profile
+            profile = UserProfile.objects.create(
+                username=username,
+                instagram=instagram,
+                twitter=twitter,
+                email=email,
+                phone_number=phone_number
+            )
+
+            # Return a success response
+            return JsonResponse({'message': 'Profile created successfully'}, status=201)
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': 'An error occurred while creating the profile'}, status=500)
+
+class GenerateQRCodeView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            username = data.get('username')
+
+            # Get the user's profile
+            profile = get_object_or_404(UserProfile, username=username)
+
+            # Generate the QR code data
+            qr_code_data = f"https://social-qr-code-backend.onrender.com/api/profiles/{profile.id}/"
+
+            # Create a QR code instance
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(qr_code_data)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+
+            # Convert the image to a BytesIO object
+            qr_img = io.BytesIO()
+            img.save(qr_img, "PNG")
+
+            # Return the URL of the generated QR code
+            return JsonResponse({'qr_code_url': qr_img.getvalue()}, status=200)
+        except json.JSONDecodeError as e:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': 'An error occurred while generating the QR code'}, status=500)
+
+class DisplayProfileView(View):
+    def get(self, request, username):
+        profile = get_object_or_404(UserProfile, username=username)
+        context = {
+            'user': profile.user,
+            'profile': profile,
+        }
+        return render(request, 'profile.html', context)
